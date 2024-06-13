d6a1a3dae54d68d9c88070a90dd8e096aad6af89
Ashley Armstrong <ashley@plott.co.uk>
2024-06-13 17:28:33 +0100

    Amends

---
+++ web/app/mu-plugins/acf.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Plugin Name: Advanced Custom Fields Loader
+ * Description: Loads the Advanced Custom Fields plugin from the mu-plugins directory.
+ * Author: Ashley Armstrong
+ * Version: 1.0
+ */
+
+// Load the ACF plugin.
+
+require_once WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php';
